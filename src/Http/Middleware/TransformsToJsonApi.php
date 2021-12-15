f4b15bd01647fd5fe8cce963d550851f2d0a7a9d
Yoan-Alexander Grigorov <yoan@myparcel.com>
2021-12-15 15:58:22 +0100

    :bug: Introduced meta.total_records and meta.total_pages support

--- src/Http/Middleware/TransformsToJsonApi.php
+++ src/Http/Middleware/TransformsToJsonApi.php
@@ -6,7 +6,9 @@ namespace MyParcelCom\Integration\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
 use MyParcelCom\Integration\Shipment\Shipment;
+use function array_key_exists;
 use function array_map;
 use function array_reduce;
 use function is_array;
@@ -32,21 +34,28 @@ class TransformsToJsonApi
             return $response;
         }
 
+        $items = $originalControllerResponse['items'];
+
         return new JsonResponse([
             'data' => array_map(
                 static fn(Shipment $shipment) => $shipment->transformToJsonApiArray(),
-                $originalControllerResponse
+                $items
             ),
+            'meta' => array_filter([
+                'total_records' => (int) Arr::get($originalControllerResponse, 'total_records'),
+                'total_pages'   => (int) Arr::get($originalControllerResponse, 'total_pages'),
+            ]),
         ]);
     }
 
     private function containsShipments(mixed $originalContent): bool
     {
         return is_array($originalContent)
+            && array_key_exists('items', $originalContent)
             && array_reduce(
-                $originalContent,
-                static fn(bool $prev, $item) => is_object($item) && $item instanceof Shipment,
+                $originalContent['items'],
+                static fn(bool $prev, $item) => $item instanceof Shipment,
                 false
-            );
+        );
     }
 }
