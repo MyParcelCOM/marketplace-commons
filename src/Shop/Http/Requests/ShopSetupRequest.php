e78e6b89370088970193af5cdb46b34b4c4c323c
Yoan-Alexander Grigorov <yoan@myparcel.com>
2025-01-31 11:12:31 +0100

    :ok_hand: Added optional MyParcel.com client credentials as input for shops setup

--- src/Shop/Http/Requests/ShopSetupRequest.php
+++ src/Shop/Http/Requests/ShopSetupRequest.php
@@ -11,13 +11,26 @@ class ShopSetupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'data.settings'     => 'array',
-            'data.redirect_url' => 'url',
+            'data.settings'         => 'array',
+            'data.redirect_url'     => 'url',
+            'data.mp_client'        => 'array',
+            'data.mp_client.id'     => 'required_with:data.mp_client|string|uuid',
+            'data.mp_client.secret' => 'required_with:data.mp_client|string',
         ];
     }
 
     public function redirectUrl(): ?string
     {
         return $this->input('data.redirect_url');
+    }
+
+    public function mpClientId(): ?string
+    {
+        return $this->input('data.mp_client.id');
+    }
+
+    public function mpClientSecret(): ?string
+    {
+        return $this->input('data.mp_client.secret');
     }
 }
---
+++ tests/Shop/Http/Requests/ShopSetupRequestTest.php
@@ -0,0 +1,150 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Shop\Http\Requests;
+
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\Validator;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use MyParcelCom\Integration\Shop\Http\Requests\ShopSetupRequest;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertTrue;
+
+class ShopSetupRequestTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public static function success_data(): array
+    {
+        return [
+            [[]],
+            [['data' => []]],
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'redirect_url' => 'https://example.com',
+                        'mp_client'    => ['id' => '', 'secret' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'redirect_url' => 'https://example.com',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public static function failure_data(): array
+    {
+        return [
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'redirect_url' => 'https://example.com',
+                        'mp_client'    => ['id' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'redirect_url' => 'https://example.com',
+                        'mp_client'    => ['secret' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'mp_client'    => ['id' => '', 'secret' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'settings'  => [
+                            'foo' => 'bar',
+                        ],
+                        'mp_client' => ['id' => '', 'secret' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'redirect_url' => 'https://example.com',
+                        'mp_client'    => ['id' => '', 'secret' => ''],
+                    ],
+                ],
+            ],
+            [
+                [
+                    'data' => [
+                        'settings'     => [
+                            'foo' => 'bar',
+                        ],
+                        'redirect_url' => 'https://example.com',
+                        'mp_client'    => ['id' => '', 'secret' => ''],
+                        'foo'          => 'bar',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    #[DataProvider('success_data')]
+    public function test_validation_rules_pass(): void
+    {
+        $request = new ShopSetupRequest();
+        $rules = $request->rules();
+
+        $validator = new Validator(
+            Mockery::mock(Translator::class),
+            ['data' => []],
+            $rules,
+        );
+
+        assertTrue($validator->passes());
+    }
+
+    #[DataProvider('failure_data')]
+    public function test_validation_rules_fail(array $data): void
+    {
+        $request = new ShopSetupRequest();
+        $rules = $request->rules();
+
+        $translator = Mockery::mock(Translator::class);
+        $translator->allows('get')->andReturn('test');
+
+        $validator = new Validator(
+            $translator,
+            $data,
+            $rules,
+        );
+
+        assertTrue($validator->fails());
+    }
+}
